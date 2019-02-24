d96fb476c73b31ede7208d259d4236a711fa4e4e
Baptiste Langlade <baptouuuu@gmail.com>
2019-02-24 15:25:15 +0100

    add watch select

---
+++ src/Watch/Select.php
@@ -0,0 +1,198 @@
+<?php
+declare(strict_types = 1);
+
+namespace Innmind\Stream\Watch;
+
+use Innmind\Stream\{
+    Watch,
+    Selectable,
+    Exception\SelectFailed
+};
+use Innmind\TimeContinuum\ElapsedPeriod;
+use Innmind\Immutable\{
+    MapInterface,
+    Map,
+    SetInterface,
+    Set
+};
+
+final class Select implements Watch
+{
+    private $timeout;
+    private $read;
+    private $write;
+    private $outOfBand;
+    private $readResources;
+    private $writeResources;
+    private $outOfBandResources;
+
+    public function __construct(ElapsedPeriod $timeout)
+    {
+        $this->timeout = $timeout;
+        $this->read = new Map('resource', Selectable::class);
+        $this->write = new Map('resource', Selectable::class);
+        $this->outOfBand = new Map('resource', Selectable::class);
+        $this->readResources = [];
+        $this->writeResources = [];
+        $this->outOfBandResources = [];
+    }
+
+    public function forRead(Selectable $read, Selectable ...$reads): Watch
+    {
+        $self = clone $this;
+        $self->read = $self->read->put(
+            $read->resource(),
+            $read
+        );
+        $self->readResources[] = $read->resource();
+
+        foreach ($reads as $read) {
+            $self->read = $self->read->put(
+                $read->resource(),
+                $read
+            );
+            $self->readResources[] = $read->resource();
+        }
+
+        return $self;
+    }
+
+    public function forWrite(Selectable $write, Selectable ...$writes): Watch
+    {
+        $self = clone $this;
+        $self->write = $self->write->put(
+            $write->resource(),
+            $write
+        );
+        $self->writeResources[] = $write->resource();
+
+        foreach ($writes as $write) {
+            $self->write = $self->write->put(
+                $write->resource(),
+                $write
+            );
+            $self->writeResources[] = $write->resource();
+        }
+
+        return $self;
+    }
+
+    public function forOutOfBand(
+        Selectable $outOfBand,
+        Selectable ...$outOfBands
+    ): Watch {
+        $self = clone $this;
+        $self->outOfBand = $self->outOfBand->put(
+            $outOfBand->resource(),
+            $outOfBand
+        );
+        $self->outOfBandResources[] = $outOfBand->resource();
+
+        foreach ($outOfBands as $outOfBand) {
+            $self->outOfBand = $self->outOfBand->put(
+                $outOfBand->resource(),
+                $outOfBand
+            );
+            $self->outOfBandResources[] = $outOfBand->resource();
+        }
+
+        return $self;
+    }
+
+    public function unwatch(Selectable $stream): Watch
+    {
+        $resource = $stream->resource();
+        $self = clone $this;
+        $self->read = $self->read->remove($resource);
+        $self->write = $self->write->remove($resource);
+        $self->outOfBand = $self->outOfBand->remove($resource);
+        $self->readResources = \array_filter(
+            $self->readResources,
+            static function($read) use ($resource): bool {
+                return $read !== $resource;
+            }
+        );
+        $self->writeResources = \array_filter(
+            $self->writeResources,
+            static function($write) use ($resource): bool {
+                return $write !== $resource;
+            }
+        );
+        $self->outOfBandResources = \array_filter(
+            $self->outOfBandResources,
+            static function($outOfBand) use ($resource): bool {
+                return $outOfBand !== $resource;
+            }
+        );
+
+        return $self;
+    }
+
+    public function __invoke(): Ready
+    {
+        if (
+            $this->read->empty() &&
+            $this->write->empty() &&
+            $this->outOfBand->empty()
+        ) {
+            return new Ready(
+                new Set(Selectable::class),
+                new Set(Selectable::class),
+                new Set(Selectable::class)
+            );
+        }
+
+        $read = $this->readResources;
+        $write = $this->writeResources;
+        $outOfBand = $this->outOfBandResources;
+        $seconds = (int) ($this->timeout->milliseconds() / 1000);
+        $microseconds = ($this->timeout->milliseconds() - ($seconds * 1000)) * 1000;
+
+        $return = @\stream_select(
+            $read,
+            $write,
+            $outOfBand,
+            $seconds,
+            $microseconds
+        );
+
+        if ($return === false) {
+            $error = \error_get_last();
+
+            throw new SelectFailed(
+                $error['message'],
+                $error['type']
+            );
+        }
+
+        return new Ready(
+            \array_reduce(
+                $read,
+                function(SetInterface $carry, $resource): SetInterface {
+                    return $carry->add(
+                        $this->read->get($resource)
+                    );
+                },
+                new Set(Selectable::class)
+            ),
+            \array_reduce(
+                $write,
+                function(SetInterface $carry, $resource): SetInterface {
+                    return $carry->add(
+                        $this->write->get($resource)
+                    );
+                },
+                new Set(Selectable::class)
+            ),
+            \array_reduce(
+                $outOfBand,
+                function(SetInterface $carry, $resource): SetInterface {
+                    return $carry->add(
+                        $this->outOfBand->get($resource)
+                    );
+                },
+                new Set(Selectable::class)
+            )
+        );
+    }
+}
---
+++ tests/Watch/SelectTest.php
@@ -0,0 +1,196 @@
+<?php
+declare(strict_types = 1);
+
+namespace Tests\Innmind\Stream\Watch;
+
+use Innmind\Stream\{
+    Watch\Select,
+    Watch\Ready,
+    Watch,
+    Selectable
+};
+use Innmind\TimeContinuum\ElapsedPeriod;
+use Innmind\Immutable\{
+    MapInterface,
+    SetInterface
+};
+use Symfony\Component\Process\Process;
+use PHPUnit\Framework\TestCase;
+
+class SelectTest extends TestCase
+{
+    private $read;
+    private $write;
+    private $oob;
+
+    public function setUp()
+    {
+        $this->read = new Process(['php', 'fixtures/read.php']);
+        $this->write = new Process(['php', 'fixtures/write.php']);
+        $this->oob = new Process(['php', 'fixtures/oob.php']);
+        $this->read = $this->read->start();
+        $this->write = $this->write->start();
+        $this->oob = $this->oob->start();
+        sleep(1);
+    }
+
+    public function tearDown()
+    {
+        try {
+            $this->read->stop(10, SIGKILL);
+            $this->write->stop(10, SIGKILL);
+            $this->oob->stop(10, SIGKILL);
+        } catch (\Throwable $e) {
+            //pass
+        }
+    }
+
+    public function testInterface()
+    {
+        $this->assertInstanceOf(
+            Watch::class,
+            new Select(new ElapsedPeriod(0))
+        );
+    }
+
+    public function testForRead()
+    {
+        $select = new Select(new ElapsedPeriod(0));
+        $resource = fopen('php://temp', 'w');
+        $stream = $this->createMock(Selectable::class);
+        $stream
+            ->expects($this->exactly(2))
+            ->method('resource')
+            ->willReturn($resource);
+
+        $select2 = $select->forRead($stream);
+
+        $this->assertInstanceOf(Select::class, $select2);
+        $this->assertNotSame($select2, $select);
+    }
+
+    public function testForWrite()
+    {
+        $select = new Select(new ElapsedPeriod(0));
+        $resource = fopen('php://temp', 'w');
+        $stream = $this->createMock(Selectable::class);
+        $stream
+            ->expects($this->exactly(2))
+            ->method('resource')
+            ->willReturn($resource);
+
+        $select2 = $select->forWrite($stream);
+
+        $this->assertInstanceOf(Select::class, $select2);
+        $this->assertNotSame($select2, $select);
+    }
+
+    public function testForOutOfBand()
+    {
+        $select = new Select(new ElapsedPeriod(0));
+        $resource = fopen('php://temp', 'w');
+        $stream = $this->createMock(Selectable::class);
+        $stream
+            ->expects($this->exactly(2))
+            ->method('resource')
+            ->willReturn($resource);
+
+        $select2 = $select->forOutOfBand($stream);
+
+        $this->assertInstanceOf(Select::class, $select2);
+        $this->assertNotSame($select2, $select);
+    }
+
+    public function testInvokeWhenNoStream()
+    {
+        $ready = (new Select(new ElapsedPeriod(0)))();
+
+        $this->assertInstanceOf(Ready::class, $ready);
+        $this->assertCount(0, $ready->toRead());
+        $this->assertCount(0, $ready->toWrite());
+        $this->assertCount(0, $ready->toOutOfBand());
+    }
+
+    public function testInvoke()
+    {
+        $read = $this->createMock(Selectable::class);
+        $read
+            ->expects($this->exactly(2))
+            ->method('resource')
+            ->willReturn($readSocket = stream_socket_client('unix:///tmp/read.sock'));
+        $write = $this->createMock(Selectable::class);
+        $write
+            ->expects($this->exactly(2))
+            ->method('resource')
+            ->willReturn($writeSocket = stream_socket_client('unix:///tmp/write.sock'));
+        $outOfBand = $this->createMock(Selectable::class);
+        $outOfBand
+            ->expects($this->exactly(2))
+            ->method('resource')
+            ->willReturn($oobSocket = stream_socket_client('tcp://127.0.0.1:1234'));
+        $select = (new Select(new ElapsedPeriod(0)))
+            ->forRead($read)
+            ->forWrite($write)
+            ->forOutOfBand($outOfBand);
+        fwrite($readSocket, 'foo');
+        fwrite($writeSocket, 'foo');
+        stream_socket_sendto($oobSocket, 'foo', STREAM_OOB);
+
+        $ready = $select();
+
+        $this->assertInstanceOf(Ready::class, $ready);
+        $this->assertCount(1, $ready->toRead());
+        $this->assertCount(1, $ready->toWrite());
+        $this->assertCount(1, $ready->toOutOfBand());
+        $this->assertSame($read, $ready->toRead()->current());
+        $this->assertSame($write, $ready->toWrite()->current());
+        $this->assertSame($outOfBand, $ready->toOutOfBand()->current());
+
+        $ready = $select();
+
+        $this->assertCount(1, $ready->toRead());
+        $this->assertCount(1, $ready->toWrite());
+        $this->assertCount(1, $ready->toOutOfBand());
+    }
+
+    public function testUnwatch()
+    {
+        $read = $this->createMock(Selectable::class);
+        $read
+            ->expects($this->exactly(3))
+            ->method('resource')
+            ->willReturn($readSocket = stream_socket_client('unix:///tmp/read.sock'));
+        $write = $this->createMock(Selectable::class);
+        $write
+            ->expects($this->exactly(3))
+            ->method('resource')
+            ->willReturn($writeSocket = stream_socket_client('unix:///tmp/write.sock'));
+        $outOfBand = $this->createMock(Selectable::class);
+        $outOfBand
+            ->expects($this->exactly(3))
+            ->method('resource')
+            ->willReturn($oobSocket = stream_socket_client('tcp://127.0.0.1:1234'));
+        $select = (new Select(new ElapsedPeriod(0)))
+            ->forRead($read)
+            ->forWrite($write)
+            ->forOutOfBand($outOfBand);
+        fwrite($readSocket, 'foo');
+        fwrite($writeSocket, 'foo');
+        stream_socket_sendto($oobSocket, 'foo', STREAM_OOB);
+
+        $select();
+        $select2 = $select
+            ->unwatch($read)
+            ->unwatch($write)
+            ->unwatch($outOfBand);
+
+        $this->assertInstanceOf(Select::class, $select);
+        $this->assertNotSame($select2, $select);
+
+        $streams = $select2();
+
+        $this->assertCount(0, $streams->toRead());
+        $this->assertCount(0, $streams->toWrite());
+        $this->assertCount(0, $streams->toOutOfBand());
+    }
+}

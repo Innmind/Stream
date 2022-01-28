e60713f18e051645369dba88766de1ec18f649b3
Baptiste Langlade <baptouuuu@gmail.com>
2022-01-28 16:04:17 +0100

    remove pre-computing of size string representation

--- src/Stream/Size.php
+++ src/Stream/Size.php
@@ -7,15 +7,7 @@ use Innmind\Stream\Exception\SizeCantBeNegative;
 
 final class Size
 {
-    private const BYTES = 1024;
-    private const KILOBYTES = 1024 ** 2;
-    private const MEGABYTES = 1024 ** 3;
-    private const GIGABYTES = 1024 ** 4;
-    private const TERABYTES = 1024 ** 5;
-    private const PETABYTES = 1024 ** 6;
-
     private int $value;
-    private string $string;
 
     public function __construct(int $value)
     {
@@ -24,48 +16,6 @@ final class Size
         }
 
         $this->value = $value;
-        $this->string = $value.'B';
-
-        switch (true) {
-            case $value < self::BYTES:
-                $this->string = $value.'B';
-                break;
-
-            case $value < self::KILOBYTES:
-                $this->string = \sprintf(
-                    '%sKB',
-                    \round($value / self::BYTES, 3),
-                );
-                break;
-
-            case $value < self::MEGABYTES:
-                $this->string = \sprintf(
-                    '%sMB',
-                    \round($value / self::KILOBYTES, 3),
-                );
-                break;
-
-            case $value < self::GIGABYTES:
-                $this->string = \sprintf(
-                    '%sGB',
-                    \round($value / self::MEGABYTES, 3),
-                );
-                break;
-
-            case $value < self::TERABYTES:
-                $this->string = \sprintf(
-                    '%sTB',
-                    \round($value / self::GIGABYTES, 3),
-                );
-                break;
-
-            case $value < self::PETABYTES:
-                $this->string = \sprintf(
-                    '%sPB',
-                    \round($value / self::TERABYTES, 3),
-                );
-                break;
-        }
     }
 
     public function toInt(): int
@@ -73,8 +23,13 @@ final class Size
         return $this->value;
     }
 
+    public function unit(): Size\Unit
+    {
+        return Size\Unit::for($this->value);
+    }
+
     public function toString(): string
     {
-        return $this->string;
+        return Size\Unit::format($this->value);
     }
 }
